737e3e7cd0d6fdd1c6aa14b86cc9ae03277a303e
ChieftainY2k <ChieftainY2k@gmail.com>
2016-03-10 15:26:45 +0100

    update

--- public/classes.php
+++ public/classes.php
@@ -5,10 +5,28 @@ class Mailer
     public function mail($recipient, $content)
     {
         // send an email to the recipient
-        echo "Executing ".__CLASS__."::".__METHOD__."\n";
+        echo "Executing ".__CLASS__."::".__METHOD__."($recipient, $content)\n";
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+interface LoggerInterface
+{
+    public function log($message);
+}
+
+class Logger implements LoggerInterface
+{
+    public function log($message)
+    {
+        // send an email to the recipient
+        echo "Executing ".__CLASS__."::".__METHOD__."($message)\n";
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
 
 class UserManager
 {
@@ -22,7 +40,20 @@ class UserManager
 
     public function register($email, $password)
     {
-        echo "Executing ".__CLASS__."::".__METHOD__."\n";
+        echo "Executing ".__CLASS__."::".__METHOD__."($email,$password)\n";
         $this->mailer->mail($email, 'Hello and welcome!');
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+class MyController
+{
+    private $userManager;
+
+    public function __construct(UserManager $userManager, LoggerInterface $logger)
+    {
+        echo "Executing ".__CLASS__."::".__METHOD__."\n";
+        $this->userManager = $userManager;
     }
 }
