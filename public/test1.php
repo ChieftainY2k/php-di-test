2c2e4e7babe2d5fba3bd8ef2b43b238dfeed2959
ChieftainY2k <ChieftainY2k@gmail.com>
2016-03-10 14:43:31 +0100

    update

--- public/test1.php
+++ public/test1.php
@@ -1,2 +1,40 @@
 <?php
 
+require "../vendor/autoload.php";
+
+
+class Mailer
+{
+    public function mail($recipient, $content)
+    {
+        // send an email to the recipient
+        echo "Executing ".__CLASS__."::".__METHOD__."\n";
+    }
+}
+
+
+class UserManager
+{
+    private $mailer;
+
+    public function __construct(Mailer $mailer)
+    {
+        echo "Executing ".__CLASS__."::".__METHOD__."\n";
+        $this->mailer = $mailer;
+    }
+
+    public function register($email, $password)
+    {
+        echo "Executing ".__CLASS__."::".__METHOD__."\n";
+        $this->mailer->mail($email, 'Hello and welcome!');
+    }
+}
+
+
+$container = DI\ContainerBuilder::buildDevContainer();
+
+$userManager = $container->get(UserManager::class);
+$userManager->register("ala@makota.pl","abcd");
+//print_r($userManager);
+
+echo "Finished.\n";
